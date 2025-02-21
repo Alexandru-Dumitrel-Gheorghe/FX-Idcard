d3930f5463699fb0162a8ce17b8210ed32c9d8f3
Alexandru Dumitrel Gheorghe <gheorghe.93@icloud.com>
2025-02-21 14:36:20 +0100

    git commit -m "Fix: Rezolvată suprapunerea textului în containerul name-cont"

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,23 @@
 $(document).ready(function () {
-    $(".id-card").hide();
-    $(".photograph").hide();
-    $(".printphoto").hide();
-    $(".create").hide();
-    $(".previewcreate-photo").hide();
-    var setIllegal = false
+    // Comentăm liniile de ascundere, astfel încât elementele să rămână vizibile
+    // $(".id-card").hide();
+    // $(".photograph").hide();
+    // $(".printphoto").hide();
+    // $(".create").hide();
+    // $(".previewcreate-photo").hide();
+    
+    // Declarăm variabilele o singură dată
+    let ShowPhoto = false;
+    let ShowIdCard = false;
+    let setIllegal = false;
+
     function setupIDCard(array) {
         if (!array || typeof array !== 'object') {
-            console.error('ID Card data is invalid:', array);
+            console.error('Date card de identitate invalide:', array);
             return;
         }
     
-        var sex = array.sex === "Female" ? "F" : "M";
+        const sex = array.sex === "Female" ? "F" : "M";
         $(".charid").html(array.charid || "N/A");
         $(".license").html(`FIXITFY-${array.charid || "N/A"}`);
         $(".sex").html(sex);
@@ -31,36 +37,39 @@ $(document).ready(function () {
             .removeClass("animate__animated animate__fadeOutRight")
             .addClass("animate__animated animate__fadeInRight")
             .show();
+    
+        ShowIdCard = true;
     }
     
-    
-    function closeIDCard() {    
-        ShowIdCard = false;  
+    function closeIDCard() {
+        ShowIdCard = false;
         $(".id-card")
             .removeClass("animate__animated animate__fadeInRight")
             .addClass("animate__animated animate__fadeOutRight")
-            .one('animationend', function() {
+            .one("animationend", function () {
                 $(this).hide();
             });
     }
     
     $("#submit").click(function () {
-        var name = $("#name").val();
-        var cityname = $("#cityname").val();
-        var religious = $("#religious").val();
-        var age = $("#ageinput").val();
-        var dateinput = $("#dateinput").val();
-        var heightinput = $("#heightinput").val();
-        var weightinput = $("#weightinput").val();
-        var hair = $("#hair").val();
-        var eye = $("#eye").val();
-        var sex = "Male";
-        var itemId = $("#previewphoto").attr("data-itemid"); 
+        const name = $("#name").val();
+        const cityname = $("#cityname").val();
+        const religious = $("#religious").val();
+        const age = $("#ageinput").val();
+        const dateinput = $("#dateinput").val();
+        const heightinput = $("#heightinput").val();
+        const weightinput = $("#weightinput").val();
+        const hair = $("#hair").val();
+        const eye = $("#eye").val();
+        let sex = "Male";
+        const itemId = $("#previewphoto").attr("data-itemid");
         console.log("Item ID:", itemId);
-        if ($("#sex-women").prop('checked')==true){
+    
+        if ($("#sex-women").prop("checked") === true) {
             sex = "Female";
         }
-        var img = $('#previewphoto').attr('src');
+    
+        const img = $("#previewphoto").attr("src");
         $.post(`https://${GetParentResourceName()}/createIdCard`, JSON.stringify({
             name: name,
             cityname: cityname,
@@ -79,6 +88,7 @@ $(document).ready(function () {
         closePrintPhoto();
         $.post(`https://${GetParentResourceName()}/close`, JSON.stringify({}));
     });
+    
     function CreateIdCardSetData(data, illegal) {
         setIllegal = illegal;
         $("#name").val(data.name);
@@ -96,24 +106,24 @@ $(document).ready(function () {
         }
     
         $("#sex-man, #sex-women").change(function () {
-            var checkedCheckbox = $(this);
+            const checkedCheckbox = $(this);
             $("#sex-man, #sex-women").not(checkedCheckbox).prop("checked", false);
         });
     
         if (!illegal) {
-            var maxYear = 1899 - data.age;
-            var maxDate = maxYear + "-12-31";
-            var minDate = maxYear + "-01-01";
+            const maxYear = 1899 - data.age;
+            const maxDate = maxYear + "-12-31";
+            const minDate = maxYear + "-01-01";
             $("#dateinput").attr("max", maxDate);
             $("#dateinput").attr("min", minDate);
             $("#dateinput").val(maxYear + "-01-01");
         }
-        console.log(JSON.stringify(data))
+        console.log(JSON.stringify(data));
         $("#previewphoto")
-            .attr("src", data.img) 
-            .attr("data-itemid", data.itemId); 
+            .attr("src", data.img)
+            .attr("data-itemid", data.itemId);
     
-        var heightText = "";
+        let heightText = "";
         switch (data.height) {
             case 0.85:
                 heightText = "4'8";
@@ -137,51 +147,47 @@ $(document).ready(function () {
                 heightText = "5'0";
                 break;
         }
-    
         $("#heightinput").val(heightText);
     }
     
     function showPrintPhoto(img) {
-        ShowPhoto = true; 
+        ShowPhoto = true;
         $(".photograph .photo").attr("src", img);
         $(".photograph").fadeIn(500);
     }
     
     function closePrintPhoto() {
-        ShowPhoto = false;  
+        ShowPhoto = false;
         $(".photograph").fadeOut(500);
         $(".printphoto").fadeOut(500);
         $(".create").fadeOut(500);
         $(".previewcreate-photo").fadeOut(500);
     }
+    
     $(".preview").click(function () {
-        var imgLink = $("#link").val();
+        const imgLink = $("#link").val();
         if (imgLink) {
             $(".photo").attr("src", imgLink);
         } else {
-            $.post(`https://${GetParentResourceName()}/notify`, JSON.stringify({
-                text: "noimg"
-            }));
+            $.post(`https://${GetParentResourceName()}/notify`, JSON.stringify({ text: "noimg" }));
         }
     });
+    
     $(".close, .close-create").click(function () {
         closePrintPhoto();
         $.post(`https://${GetParentResourceName()}/close`, JSON.stringify({}));
     });
+    
     $(".print").click(function () {
-        var imgLink = $("#link").val();
+        const imgLink = $("#link").val();
         if (imgLink) {
             closePrintPhoto();
-            $.post(`https://${GetParentResourceName()}/print`, JSON.stringify({
-                imgLink: imgLink
-            }));
+            $.post(`https://${GetParentResourceName()}/print`, JSON.stringify({ imgLink: imgLink }));
         } else {
-            $.post(`https://${GetParentResourceName()}/notify`, JSON.stringify({
-                text: "noimg"
-            }));
+            $.post(`https://${GetParentResourceName()}/notify`, JSON.stringify({ text: "noimg" }));
         }
     });
-
+    
     $(document).keyup(function (e) {
         if (e.key === "Escape") {
             let isClosed = false;
@@ -202,28 +208,25 @@ $(document).ready(function () {
         }
     });
     
-    let ShowPhoto = false;
-    let ShowIdCard = false;
-    
-    window.addEventListener('message', function (event) {
+    window.addEventListener("message", function (event) {
         switch (event.data.action) {
-            case 'openIdCard':
+            case "openIdCard":
                 ShowIdCard = true;
                 setupIDCard(event.data.array);
                 break;
-            case 'close':
+            case "close":
                 closeIDCard();
                 break;
-            case 'print':
+            case "print":
                 $(".printphoto").fadeIn(500);
                 break;
-            case 'showphoto':
-                var img = event.data.array.img
+            case "showphoto":
+                const img = event.data.array.img;
                 ShowPhoto = true;
                 showPrintPhoto(img);
                 break;
-            case 'createidcard':
-                var data = event.data.array
+            case "createidcard":
+                const data = event.data.array;
                 if (event.data.illegal === true) {
                     $("#cityname").removeAttr("disabled");
                     $("#heightinput").removeAttr("disabled");
@@ -233,7 +236,7 @@ $(document).ready(function () {
                     $("#dateinput").removeAttr("min");
                     $("#dateinput").removeAttr("max");
                 }
-                CreateIdCardSetData(data, event.data.illegal)
+                CreateIdCardSetData(data, event.data.illegal);
                 $(".create").fadeIn(500);
                 $(".previewcreate-photo").fadeIn(500);
                 break;
